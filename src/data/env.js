568fe42f11a0246adf30ce83c808a61226eea922
Oleg <oleg.pav.m@gmail.com>
2018-10-17 12:49:11 +0300

    Stock skeleton.

--- src/data/env.js
+++ src/data/env.js
@@ -1,5 +1,6 @@
 const env = process.env;
 
 module.exports = {
-    // -
+    PCAP_PUBLIC_KEY: env.PCAP_PUBLIC_KEY,
+    PCAP_PRIVATE_KEY: env.PCAP_PRIVATE_KEY,
 };
--- src/utils/Stock.js
+++ src/utils/Stock.js
@@ -1,5 +1,39 @@
+const request = require('request-promise-native');
+const moment = require('moment');
+const sleep = require('then-sleep');
+
 class Stock {
-    // TODO -
+    static async getOrderList() {
+        // TODO -
+    }
+
+    static async placeEnterOrder() {
+        // TODO -
+    }
+
+    static async placeExitOrder() {
+        // TODO -
+    }
+
+    static async getCandles() {
+        // TODO -
+    }
+
+    _sign() {
+        // TODO -
+    }
+
+    _makeHeaders() {
+        // TODO -
+    }
+
+    _makeExpires() {
+        // TODO -
+    }
+
+    _privateRequest() {
+        // TODO -
+    }
 }
 
 module.exports = Stock;
