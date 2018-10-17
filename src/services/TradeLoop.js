95a8162d449ca1dc7e7b8beb37feda95936fb3aa
Oleg <oleg.pav.m@gmail.com>
2018-10-17 14:07:03 +0300

    Loop draft.

--- src/services/TradeLoop.js
+++ src/services/TradeLoop.js
@@ -1,5 +1,8 @@
+const sleep = require('then-sleep');
 const core = require('gls-core-service');
 const BasicService = core.services.Basic;
+const Logger = core.utils.Logger;
+const env = require('../data/env');
 
 class TradeLoop extends BasicService {
     constructor({ type, edge, price, amount }) {
@@ -12,6 +15,19 @@ class TradeLoop extends BasicService {
     }
 
     async start() {
+        setImmediate(async () => {
+            while (true) {
+                try {
+                    await this._iteration();
+                } catch (error) {
+                    Logger.error(error);
+                }
+                await sleep(env.PCAP_LOOP_TIME);
+            }
+        });
+    }
+
+    async _iteration() {
         // TODO -
     }
 }
