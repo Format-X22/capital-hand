1bca025b24af23e34a3ca6412d9cb79d6959be86
Oleg <oleg.pav.m@gmail.com>
2018-10-17 13:49:09 +0300

    Get candles.

--- src/data/env.js
+++ src/data/env.js
@@ -3,4 +3,5 @@ const env = process.env;
 module.exports = {
     PCAP_PUBLIC_KEY: env.PCAP_PUBLIC_KEY,
     PCAP_PRIVATE_KEY: env.PCAP_PRIVATE_KEY,
+    PCAP_RETRY_TIME: env.PCAP_RETRY_TIME || 5000,
 };
--- src/utils/Stock.js
+++ src/utils/Stock.js
@@ -1,6 +1,9 @@
 const request = require('request-promise-native');
 const moment = require('moment');
 const sleep = require('then-sleep');
+const core = require('gls-core-service');
+const Logger = core.utils.Logger;
+const env = require('../data/env');
 
 class Stock {
     static async getOrderList() {
@@ -16,22 +19,67 @@ class Stock {
     }
 
     static async getCandles() {
+        let response;
+        let result;
+
+        try {
+            response = await request(this._makeCandlesRequestConfig());
+
+            if (!response['t']) {
+                throw 'Bad candles data';
+            }
+
+            result = this._convertCandlesToArray(response);
+        } catch (err) {
+            Logger.error(`Fail to load candles, sleep and retry... ${err}`);
+            await sleep(env.PCAP_RETRY_TIME);
+
+            return await this.getCandles();
+        }
+
+        return result;
+    }
+
+    static _makeCandlesRequestConfig() {
+        return {
+            uri: 'https://www.bitmex.com/api/udf/history',
+            qs: {
+                symbol: 'XBTUSD',
+                from: this._toSeconds(moment().subtract(7, 'd')),
+                to: this._toSeconds(moment()),
+                resolution: '1D',
+            },
+            json: true,
+        };
+    }
+
+    static _toSeconds(val) {
+        return (val / 1000) | 0;
+    }
+
+    static _convertCandlesToArray(raw) {
+        const result = [];
+
+        for (let i = 0; i < raw['t'].length; i++) {
+            result.push([raw['t'][i], raw['o'][i], raw['h'][i], raw['l'][i], raw['c'][i]]);
+        }
+
+        return result;
+    }
+
+    static _sign() {
         // TODO -
     }
 
-    _sign() {
+    static _makeHeaders() {
         // TODO -
     }
 
-    _makeHeaders() {
+    static _makeExpires() {
         // TODO -
     }
 
-    _makeExpires() {
-        // TODO -
-    }
-
-    _privateRequest() {
+    static async _privateRequest() {
         // TODO -
     }
 }
